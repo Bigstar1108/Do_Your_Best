47b61e68d8447fa78d010efca1b4622225649f2b
bigstar7966 <daesung09854@gmail.com>
2020-07-14 19:53:25 +0900

    [Del]
    delete counter action and reducer

--- scorecounter/src/modules/actions/index.js
+++
@@ -1,9 +0,0 @@
-import betinfoAction from './betinfo.actions';
-import counterAction from './counter.action';
-
-const allActions = {
-    betinfoAction,
-    counterAction
-};
-
-export default allActions;
