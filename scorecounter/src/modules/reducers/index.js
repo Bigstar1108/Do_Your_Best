0cd717e58b1fe8a9ecba9e96d1426fa4738fc095
bigstar7966 <daesung09854@gmail.com>
2020-07-14 19:54:18 +0900

    [Fix]
    delete counter reducer

--- scorecounter/src/modules/reducers/index.js
+++ scorecounter/src/modules/reducers/index.js
@@ -1,9 +1,7 @@
-import counter from '../reducers/counter.reducer';
 import betinfo from '../reducers/betinfo.reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-    counter,
     betinfo
 });
 
