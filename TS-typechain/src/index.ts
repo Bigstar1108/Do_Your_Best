8e2a027fbb6928377f8a16b61ae95666a99839ed
Bigstar1108 <daesung09854@gmail.com>
2020-07-20 21:43:42 +0900

    [Fix]

--- TS-typechain/src/index.ts
+++ TS-typechain/src/index.ts
@@ -1,19 +1,110 @@
-interface Human{
-    name: string,
-    age: number,
-    gender: string
+import * as CryptoJS from 'crypto-js';
+
+class Block{
+    //Hash키를 생성하기 위한 메소드 (static으로 선언함으로서 class를 선언하지 않아도 사용 가능)
+    static calculateBlockHash = (
+        index:number, 
+        previousHash:string, 
+        timestamp:number, 
+        data:string) : string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    
+    //블록의 구조를 인증하기 위한 메소드
+    static validateStructure = (aBlock : Block) : boolean => 
+        typeof aBlock.index === "number" && 
+        typeof aBlock.hash === "string" && 
+        typeof aBlock.previousHash === "string" && 
+        typeof aBlock.timestamp === "number" && 
+        typeof aBlock.data === "string";
+
+    public index:number;
+    public hash:string;
+    public previousHash:string;
+    public data:string;
+    public timestamp:number;
+
+    constructor(
+        index:number,
+        hash:string,
+        previousHash:string,
+        data:string,
+        timestamp:number
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+
+const firstBlock:Block = new Block(0, "2020202020202", "", "Hello", 123456);
+
+let blockChain:Block[] = [firstBlock];
+
+//함수의 return 값이 배열임으로 Block[]
+const getBlockChain = () : Block[] => blockChain;
+
+//블록체인 배열안에 있는 최신 블록을 가져오는 함수
+const getLatesBlock = () : Block => blockChain[blockChain.length - 1];
+
+//새로운 timestamp를 받아오는 함수
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+
+//새로운 블록을 만드는 함수
+const createNewBlock = (data:string) : Block => {
+    const previousBlock : Block = getLatesBlock();
+    const newIndex : number = previousBlock.index + 1;
+    const newTimestamp : number = getNewTimeStamp();
+    const newHash : string = Block.calculateBlockHash(
+        newIndex,
+        previousBlock.hash, 
+        newTimestamp,
+        data
+    );
+    const newBlock : Block = new Block(
+        newIndex,
+        newHash,
+        previousBlock.hash,
+        data,
+        newTimestamp
+    );
+    addBlock(newBlock);
+    return newBlock;
 };
 
-const person = {
-    name : "daeseong",
-    age : 19,
-    gender : "male"
+// 각 블록의 해쉬키를 발급 받는 함수
+const getHashforBlock = (aBlock : Block) :string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
+// 블록이 유효한지 체크하는 함수
+const isBlockValid = (candidateBlock : Block, previousBlock : Block) : boolean => {
+    //블록이 유효한지 체크
+    if(!Block.validateStructure(candidateBlock)){
+        return false;
+    } //이전 블록의 인덱스 + 1과 현재 블록의 인덱스가 같은지 체크
+    else if(previousBlock.index + 1 !== candidateBlock.index){
+        return false;
+    } //이전 블록의 해쉬와 현재 블록의 이전해쉬가 같은지 체크
+    else if(previousBlock.hash !== candidateBlock.previousHash){
+        return false;
+    } //현재 블록의 해쉬키와 현재블록이 가지고 있는 해쉬키가 같은지 체크
+    else if(getHashforBlock(candidateBlock) !== candidateBlock.hash){
+        return false;
+    }else{
+        return true;
+    }
 };
 
-const sayHi = (person:Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!`;
-};
+// 블록을 추가하는 함수
+const addBlock = (candidateBlock: Block): void => {
+    if(isBlockValid(candidateBlock, getLatesBlock())){
+        blockChain.push(candidateBlock);
+    }
+}
 
-console.log(sayHi(person));
+createNewBlock("second Block");
+createNewBlock("third Block");
+createNewBlock("fourth Block");
+
+console.log(blockChain);
 
 export {};
