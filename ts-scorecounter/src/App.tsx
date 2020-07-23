4d0722bbc6bc0b401556c43fcbd48deedca1e622
Bigstar1108 <daesung09854@gmail.com>
2020-07-23 13:47:21 +0900

    [SETTING]
    set react router dom
    
    Co-Authored-By: Daeseong Hwang <65744080+bigstar1108@users.noreply.github.com>

--- ts-scorecounter/src/App.tsx
+++ ts-scorecounter/src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import styled, { createGlobalStyle } from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>dd</h1>
-    </div>
-  );
+import Home from './pages/Home';
+import Main from './pages/Main';
+
+const RootDiv = styled.div`
+  height : 100vh;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    padding : 0;
+    margin : 0;
+  }
+`;
+
+const App = () => {
+  return(
+    <React.Fragment>
+      <GlobalStyle />
+      <RootDiv>
+        <Switch>
+          <Route exact path = "/" component = {Home} />
+          <Route exact path = "/main" component = {Main} />
+        </Switch>
+      </RootDiv>
+    </React.Fragment>
+  )
 }
 
 export default App;
